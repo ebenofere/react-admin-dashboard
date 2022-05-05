b9cdc21d59e15b7b614179a3f1514acd2efc56a4
ebenofere <eofere@gmail.com>
2022-05-06 00:34:12 +0100

    2. React sidebar menu component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </Router>
   );
 }
