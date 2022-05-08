21d3a7d3c19557bb42445ee0c2c987585e481e77
ebenofere <eofere@gmail.com>
2022-05-08 03:24:41 +0100

    6. User Information components

--- src/components/practice.js
+++ src/components/practice.js
@@ -1,26 +1,80 @@
-import Chart from "../../components/chart/Chart";
-import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
-import "./home.css";
-import { userData } from "../../dummyData";
-import WidgetSm from "../../components/widgetSm/WidgetSm";
-import WidgetLg from "../../components/widgetLg/WidgetLg";
+import "./widgetLg.css";
 
-export default function Home() {
+export default function WidgetLg() {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
   return (
-    <div className="home">
-      <FeaturedInfo />
-      <Chart
-        data={userData}
-        title="User Analytics"
-        grid
-        dataKey="Active User"
-      />
-      <div className="homeWidgets">
-        <WidgetSm />
-        <WidgetLg />
-      </div>
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Declined" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Pending" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
-
-// CHART
